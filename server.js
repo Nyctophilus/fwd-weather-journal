7e4d44bceb2e06180f859dd5d2e91b78327c2e11
Medo <mohammed.yuossry@gmail.com>
2022-03-24 14:40:12 +0200

    Setup server - base

---
+++ server.js
@@ -0,0 +1,40 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require("express");
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require("cors");
+
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static("fwd-weather-journal-app/website"));
+
+// Setup Server
+const port = 5000;
+app.listen(port, () =>
+  console.log(`server is running at localhost:${port}`)
+);
+
+// get route on '/get'
+app.get("/get", (req, res) =>
+  res.status(200).send(projectData)
+);
+
+// post route on '/add'
+app.post("/add", (req, res) => {
+  projectData.temperature = req.body.temperature;
+  projectData.date = req.body.date;
+  projectData.userResponse = req.body.userResponse;
+});
