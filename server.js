d0c9700269c11a444f7c16a191c6258e667ace4b
Medo <mohammed.yuossry@gmail.com>
2022-03-25 13:07:03 +0200

    met review feedback, Improved functional prog code

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-console.log(projectData);
-
 // Require Express to run server and routes
 const express = require("express");
 
 // Start up an instance of app
 const app = express();
+app.use(express.json());
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -17,8 +16,6 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require("cors");
-const res = require("express/lib/response");
-
 app.use(cors());
 
 // Initialize the main project folder
@@ -66,3 +63,5 @@ app.get("/apikey", (req, res) => {
     key: process.env.APIKEY,
   });
 });
+
+console.log(process.env.APIKEY);
