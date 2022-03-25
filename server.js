1d6fe44fb1914887d6c5a78af5d85c01167534cc
Medo <mohammed.yuossry@gmail.com>
2022-03-25 14:51:48 +0200

    commented reqduent middleware

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express");
 
 // Start up an instance of app
 const app = express();
-app.use(express.json());
+// app.use(express.json());  // do the same as Middleware as app.use(bodyParser.json());
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
